d165e0f1912dc3a824683fbbb993f1cb3299bdf9
bargenish44 <44568486+bargenish44@users.noreply.github.com>
2018-11-26 17:23:43 +0200

    new fix

---
+++ src/Coords/MyCoordsTest.java
@@ -0,0 +1,14 @@
+package Coords;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyCoordsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
