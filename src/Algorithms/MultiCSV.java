124b8aeddd2a7ccaefae7e0ae9bf243b72cc7319
bargenish44 <44568486+bargenish44@users.noreply.github.com>
2018-12-02 01:59:18 +0200

    Update MultiCSV.java

--- src/Algorithms/MultiCSV.java
+++ src/Algorithms/MultiCSV.java
@@ -49,8 +49,8 @@ public class MultiCSV {
 		return p;
 	}
 
-	//public static void main(String[] args) {
-		//project p=multicsv("C:\\Users\\barge\\Desktop\\‏‏תיקיה חדשה");
-		//multcsv2kml(p,"C:\\Users\\barge\\Desktop\\‏‏תיקיה חדשה");
-	//}
+	public static void main(String[] args) {
+		project p=multicsv("C:\\Users\\barge\\Desktop\\‏‏תיקיה חדשה");
+		multcsv2kml(p,"C:\\Users\\barge\\Desktop\\‏‏תיקיה חדשה");
+	}
 }
