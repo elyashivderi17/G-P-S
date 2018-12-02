97f813b533eb8febd94cba163af5c1d17b7f926a
elyashiv <elyashivderi17@gmail.com>
2018-12-02 23:05:04 +0200

    new

--- src/Coords/Tests.java
+++ src/Coords/Tests.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Test;
 import Geom.Point3D;
 
 class Tests {
+	
 	@Test
-
 	void testAdd() {
  
 		Point3D p1=new Point3D(32.103315,35.209039,670.0);
@@ -19,33 +19,30 @@ class Tests {
 
 		Point3D p3=new Point3D(337.6989921,-359.2492069,-20);
 
-		MyCoords m=new MyCoords();
+		MyCoords temp=new MyCoords();
 		
-		Point3D p4=m.add(p1, p3);
+		Point3D p4=temp.add(p1, p3);
 		
 		if(!p2.equals(p4)) {
 
 			fail("this function doesnt work well");
-
 		}
-
 	}
 
 
 
 	@Test
-
 	void testDistance3d() {
 
 		Point3D p1=new Point3D(32.103315,35.209039,670);
 
 		Point3D p2=new Point3D(32.106352,35.205225,650);
 
-		MyCoords m=new MyCoords();
+		MyCoords temp=new MyCoords();
 
 		double dis=493.0523318324134;
 
-		if((m.distance3d(p1, p2)) != dis) {
+		if((temp.distance3d(p1, p2)) != dis) {
 
 			fail("the 'Distance3d' function doesnt work well");
 
@@ -65,20 +62,17 @@ class Tests {
 
 		Point3D p3=new Point3D(337.69899206128815,-359.24920693881893,-20.0);
 
-		MyCoords m=new MyCoords();
+		MyCoords temp =new MyCoords();
 
-		if(!p3.equals(m.vector3D(p1, p2))) {
+		if(!p3.equals(temp.vector3D(p1, p2))) {
 
 			fail("the 'testVector3D' function doesnt work well");
-
 		}
-
 	}
 
 
 
 	@Test
-
 	void testAzimuth_elevation_dist() {
 
 		Point3D p1=new Point3D(32.103315,35.209039,670);
@@ -87,30 +81,25 @@ class Tests {
 
 		double [] p3= {313.2304203264989,-2.3247635173865278,493.0523318324134};
 
-		MyCoords m=new MyCoords();
+		MyCoords temp =new MyCoords();
 
-		if(!Arrays.equals(p3, m.azimuth_elevation_dist(p1, p2))) {
+		if(!Arrays.equals(p3, temp.azimuth_elevation_dist(p1, p2))) {
 
 			fail("the 'Azimuth_elevation_dist' function doesnt work well");
-
 		}
-
 	}
 
 
 
 	@Test
-
 	void testIsValid_GPS_Point() {
 
 		Point3D p1=new Point3D(32.103315,35.209039,670);
 
-		MyCoords m=new MyCoords();
+		MyCoords temp=new MyCoords();
 
-		if (!m.isValid_GPS_Point(p1))
+		if (!temp.isValid_GPS_Point(p1))
 
 			fail("the 'IsValid_GPS_Point' function doesnt work well");
-
 	}
-
 }
